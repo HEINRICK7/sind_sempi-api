7ea3ea74e9c52b0e9cd1f765d50b414c950a19f4
Henrique <Carlos Henrique>
2021-04-01 20:10:32 -0300

    update files

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv/config');
 
-mongoose.connect('mongodb://localhost/sindSempi',{ useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGO_URL,
+    { useUnifiedTopology: true, 
+      useNewUrlParser: true, 
+      useCreateIndex: true, 
+      useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
